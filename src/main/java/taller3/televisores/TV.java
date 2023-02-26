406a3b110687a5c270af080f0d36efcb4c7a2f7b
shurming01 <salashiguita.rs@gmail.com>
2023-02-26 11:18:24 -0500

    commit

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -94,7 +94,7 @@ public class TV {
 	
 	public void volumenUp() {
 		if (this.estado == true) {
-			if (volumen > 0 || volumen < 7){
+			if (volumen > 0 && volumen < 7){
 				volumen++;
 			}
 		}
@@ -102,7 +102,7 @@ public class TV {
 	
 	public void volumenDown() {
 		if (this.estado == true) {
-			if (volumen > 0 || volumen <= 7){
+			if (volumen > 0 && volumen <= 7){
 				volumen--;
 			}
 		}
